06c6b115436cc774bb57a61c63fb0100ead3b7fe
Aparna Singh <aparnasingh7731@gmail.com>
2025-08-26 20:17:16 +0000

    new

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,9 @@
+console.log("Apps270")
+const accountId = 144553
+let accountEmail = "apps270@google.com"
+var accountPassword= "12345"
+accountCity= "Jaipur"
+/*var, let, const are used to define items to the memory. Const are fixed*/
+accountId = 2
+console.log(accountId);
+
